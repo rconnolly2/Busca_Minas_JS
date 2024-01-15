7200dc404272bcd4366700b1f08aafedfa711057
Robert Connolly <robertoconnolly100@gmail.com>
2024-01-15 01:20:05 +0100

    PierdesPartida

--- codigo.js
+++ codigo.js
@@ -1,6 +1,8 @@
 var dificultad = 1;
 var dim_tabla = [8, 8];
+var lista_minas = [];
 const tabla = CrearTablaMinas(8, 8, 15);
+var juego_on = true;
 console.log(tabla);
 
 function MinaAlrededor(index_mina, tabla, lista_minas) {
@@ -26,7 +28,6 @@ function CrearTablaMinas(filas, columnas, minas) {
     // Creo una array de indices (que serán minas) por el numero de minas que me piden
     let tabla = []; // tabla final
     let minas_necesarias = minas;
-    let lista_minas = [];
     while (minas_necesarias!=0) {
         // crea minas
         lista_minas.push([Math.floor(Math.random() * filas), Math.floor(Math.random() * columnas)]);
@@ -66,10 +67,52 @@ function CrearTablaMinas(filas, columnas, minas) {
   }
 
 
+function EsPar(n) {
+    return n%2 == 0;
+}
+
+
 function crear_tabla(id_div_contenedor) {
     let div_minas = document.getElementById(id_div_contenedor);
     for (let i=0; i<=tabla.length-1; i++) {
         for (let j=0; j<=tabla[i].length-1; j++) {
+            let img_cuadrado;
+
+            if (EsPar(i+j)) { // Sumo iteradores columnas, filas y compruebo si es par:
+                              // https://stackoverflow.com/a/75585834
+                img_cuadrado = "lightblue.png";
+            } else {
+                img_cuadrado = "darkblue.png";
+            }
+
+            div_minas.innerHTML += `<img class="cuadrado" id="cuadrado" index="${i}, ${j}" src="./img/${img_cuadrado}" onclick="click_cuadrado(this)">`;
+        }
+    }
+}
+
+function SonListasIguales(lista1, lista2) {
+    let lista1_ord, lista2_ord;
+    if (lista1.length !== lista2.length) {
+        return false;
+    }
+
+    lista1_ord = lista1.sort(), lista2_ord = lista2.sort();
+
+    for (let i=0; i<=lista1_ord.length-1; i++) {
+        if (lista1_ord[i] != lista2_ord[i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function PierdesPartida() {
+    let div_minas = document.getElementById("div_minas");
+
+    for (let i=0; i<=tabla.length-1; i++) {
+        for (let j=0; j<=tabla.length-1; j++) {
+
             let img_cuadrado;
             switch (tabla[i][j]) {
                 case 0:
@@ -90,14 +133,53 @@ function crear_tabla(id_div_contenedor) {
                 default:
                     img_cuadrado = "3_lightblue.png";
 
-            }
+                }
 
-            if (tabla[i][j] == tabla[i].length-1) { // Si es el ultimo numero de la columna, pintar y añadir un br
-                div_minas.innerHTML += `<img class="cuadrado" id="cuadrado" index="${i}, ${j}" src="./img/${img_cuadrado}">`;
-                div_minas.innerHTML += "<br>";
-            } else { // Si no pintar el cuadrado que toca
-                div_minas.innerHTML += `<img class="cuadrado" id="cuadrado" index="${i}, ${j}" src="./img/${img_cuadrado}">`;
-            }
+            setTimeout(() => { // Sustituyo cada cuadrado por la imagen que tenga en tabla
+                if (!EsPar(i+j)) { // si la posición actual del iterador no es par:
+                    img_cuadrado = img_cuadrado.replace("lightblue", "darkblue"); // cambia la imagen a la version azul oscuro
+                }
+
+                const elemento_img = div_minas.querySelector(`img[index="${i}, ${j}"]`);
+
+                if (elemento_img) {
+                    elemento_img.src = `./img/${img_cuadrado}`;
+                    elemento_img.classList.add("flip_individual");
+                }
+            }, i * 800); // por cada iteración 0,8s de delay
         }
+    }
+}
+
+function click_cuadrado(elemento_html) {
+    let index_cuadrado = elemento_html.getAttribute('index').split(", "); // Consigo el atributo index y divido el string en una lista
+
+    if (tabla[index_cuadrado[0]][index_cuadrado[1]] == 9) {
+        console.log("Es mina!");
+        PierdesPartida();
+    } else {
+        let img_cuadrado;
+        switch (tabla[index_cuadrado[0]][index_cuadrado[1]]) {
+            case 0:
+                img_cuadrado = "lightblue.png";
+                break;
+            case 1:
+                img_cuadrado = "1_lightblue.png";
+                break;
+            case 2:
+                img_cuadrado = "2_lightblue.png";
+                break;
+            case 3:
+                img_cuadrado = "3_lightblue.png";
+                break;
+            case 9:
+                img_cuadrado = "mina_lightblue.png";
+                break;
+            default:
+                img_cuadrado = "3_lightblue.png";
+        }
+        elemento_html.src = "../img/" + img_cuadrado;
+        elemento_html.classList.add("flip_individual");
+
     }
 }
